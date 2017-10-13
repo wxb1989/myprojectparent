d9efd06d4341ccdc9f7dac0c2cb02a76c86c01ca
wangx <wxb1989.wxb@163.com>
2017-10-13 15:54:17 +0800

    初始化项目

---
+++ myproject-sexample/src/test/java/com/RefactorVariableTest.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.junit.Test;
+
+public class RefactorVariableTest  {
+
+    @Test
+    public void main() throws Exception {
+        System.out.println("main");
+    }
+
+    @Test
+    public void getDiscountFactor() throws Exception {
+        System.out.println("getDiscountFactor");
+    }
+}
