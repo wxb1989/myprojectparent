a2854c63cb5597d3d567d96374b526fda4eeb664
wangx <wxb1989.wxb@163.com>
2017-10-13 15:55:25 +0800

    初始化项目

---
+++ myproject-sexample/src/main/java/com/wangxuebing/annotation/AnnotationTest.java
@@ -0,0 +1,11 @@
+package com.wangxuebing.annotation;
+
+import java.io.Serializable;
+
+
+public class AnnotationTest implements Serializable {
+
+    private static final long serialVersionUID = -3210031379297872082L;
+
+    
+}
