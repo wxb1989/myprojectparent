71134eb8ea5ca52a144aef52bff375dd01ae4e18
wangx <wxb1989.wxb@163.com>
2017-10-16 15:04:09 +0800

    通过一定的方式处理请求随机访问集群中的任意一台服务器

--- myproject-sexample/src/main/java/com/wangxuebing/arithmetic/loadbalance/Random.java
+++ myproject-sexample/src/main/java/com/wangxuebing/arithmetic/loadbalance/Random.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * @author Administrator
+ * 随机方式处理
  */
 public class Random {
     public static String  getServer() {
@@ -25,6 +26,5 @@ public class Random {
 
     public static void main(String[] args) {
         System.out.println(getServer());
-
     }
 }
