6a42d517b8d58b36aa23010d6fa9610c37161379
wangx <wxb1989.wxb@163.com>
2017-11-09 10:25:11 +0800

    代理模式

---
+++ myproject-sexample/src/main/java/com/wangxuebing/design/proxy/TestB.java
@@ -0,0 +1,25 @@
+package com.wangxuebing.design.proxy;
+
+/**
+ * @author ${Administrator}
+ * @description ${DESCRIPTION}
+ * @create 2017-11-07 16:18
+ **/
+public class TestB implements TestInterface {
+
+
+    @Override
+    public void doSomeThing() {
+        System.out.println("TestB ---------- doSomeThing");
+    }
+
+    @Override
+    public void doAnyThing() {
+        System.out.println("TestB ---------- doAnyThing");
+    }
+
+    @Override
+    public void doNoThing() {
+        System.out.println("TestB ---------- doNoThing");
+    }
+}
---
+++ myproject-sexample/src/main/java/com/wangxuebing/design/proxy/TestInterface.java
@@ -0,0 +1,12 @@
+package com.wangxuebing.design.proxy;
+
+/**
+ * @author ${Administrator}
+ * @description ${DESCRIPTION}
+ * @create 2017-11-07 16:17
+ **/
+public interface TestInterface {
+    public  void doSomeThing();
+    public  void doAnyThing();
+    public  void doNoThing();
+}
