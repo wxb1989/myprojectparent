9818cbdfc5ea05dc1ffc7f8948af3a9c4b3241cb
wangx <wxb1989.wxb@163.com>
2019-03-18 13:57:14 +0800

    设计模式

---
+++ myproject-sexample/src/main/java/com/wangxuebing/other/AbstractHero.java
@@ -0,0 +1,53 @@
+package com.wangxuebing.other;
+
+/**
+ * @author ${Administrator}
+ * @package com.wangxuebing.other
+ * @description
+ * @create 2018-11-07 11:47
+ **/
+public abstract class AbstractHero {
+
+    protected final String name;//名字
+    protected final String heroAttributes;//属性
+    protected String[] skills = new String[4];//技能
+
+
+    public AbstractHero() {
+        super();
+        this.name = getName();
+        initSkills();
+        this.heroAttributes = initAttributes();
+    }
+
+    //物理攻击
+    public void commonAttack() {
+        System.out.println(name + "进行物理攻击");
+    }
+
+    //
+    protected  void checkSkills() {
+        if (this.skills.length > 4 || this.skills.length < 0) {
+            throw new IllegalArgumentException();
+        }
+        for (String skill : this.skills) {
+            if (skill == null) {
+                throw new NullPointerException();
+            }
+        }
+
+    }
+
+    protected void release(int index) {
+        if (index < 0 || index > 3) {
+
+        }
+        System.out.println(name + "释放" + skills[index]);
+    }
+
+    protected abstract String getName();
+
+    protected abstract void initSkills();
+
+    protected abstract String initAttributes();
+}
