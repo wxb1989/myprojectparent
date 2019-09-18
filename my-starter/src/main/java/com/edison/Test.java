276095a8dd38f653403dc80333357790ea9ace16
wangx <wxb1989.wxb@163.com>
2019-09-18 15:00:04 +0800

    自定义starter

---
+++ my-starter/src/main/java/com/edison/Test.java
@@ -0,0 +1,13 @@
+package com.edison;
+
+/**
+ * @author
+ * @package com.edison
+ * @description
+ * @create 2019-09-18 14:58
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
