5a18e286afaa11939b7cee19445691c0c522d216
wangx <wxb1989.wxb@163.com>
2019-09-18 15:25:36 +0800

    assembly 打包方式

--- demo/src/test/java/com/assembly/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/assembly/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.edison.starter;
+package com.assembly.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyStarterApplicationTests {
+public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
