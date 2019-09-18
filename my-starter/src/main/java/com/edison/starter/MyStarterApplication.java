88eb75692e8f65ac0e88382d0811bd7fd874c8cf
wangx <wxb1989.wxb@163.com>
2019-09-18 14:26:46 +0800

    修改redis用aop方式实现的方法

---
+++ my-starter/src/main/java/com/edison/starter/MyStarterApplication.java
@@ -0,0 +1,13 @@
+package com.edison.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStarterApplication.class, args);
+    }
+
+}
