3bb3b1d0a71673e49417aceb5d7680bbe3865e98
wangx <wxb1989.wxb@163.com>
2019-11-14 12:38:23 +0800

    自定义starter

---
+++ cloud/eraurk-server/src/main/java/com/cxjk/cloud/eurekas/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.cxjk.cloud.eurekas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author
+ * @package com.cxjk.cloud.eurekas
+ * @description
+ * @create 2019-11-14 11:54
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
